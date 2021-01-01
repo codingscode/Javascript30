1f20aceaced6c9c648bf3eb61bfc736ac7539a9c
Heronnes Pereira <heronnestg@hotmail.com>
2020-12-31 21:50:41 -0300

    parte 2

--- 06 - Type Ahead/json.js
+++ 06 - Type Ahead/json.js
@@ -1,8 +1,23 @@
-const ref = './ref/cidades.json'
+const ref = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json'
 
 const cidades = []
 
-// https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json
-const prom = fetch('https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json')
-console.log(prom)
+fetch(ref).then(blob => console.log(blob))
 
+
+
+
+
+
+
+
+/* const matriz = require('./ref/cidades.json')
+
+console.log(matriz[0]) // 1º objeto
+console.log(matriz[0].cidade)
+console.log(typeof(matriz))
+
+const x = JSON.stringify(matriz)
+//console.log(x)
+console.log(typeof(x))
+ */
