6fdd835cff818f48d99804e80ff9aeae3b093f1d
Heronnes Pereira <heronnestg@hotmail.com>
2021-01-02 21:25:20 -0300

    parte 5

--- 06 - Type Ahead/json.js
+++ 06 - Type Ahead/json.js
@@ -1,10 +1,10 @@
 const ref = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json'
 
-let cidades = []
+const cidades = []
 
 fetch(ref)
       .then(cada => cada.json())
-      .then(dados => cidades = dados)
+      .then(dados => cidades.push(dados))
 
 
 
